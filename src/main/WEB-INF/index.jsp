56b69f8bb11e91e5b6da9ee20134b7cfa2e93c31
dthomas <dimitrithomas@web.de>
2019-08-14 15:52:41 +0200

    Initial commit.

---
+++ src/main/WEB-INF/index.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Marslander Algo</title>
+</head>
+<body>
+Test bla!
+</body>
+</html>
---
+++ src/main/java/de/dthomas/marslander/MarslanderApplication.java
@@ -0,0 +1,13 @@
+package de.dthomas.marslander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarslanderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MarslanderApplication.class, args);
+  }
+
+}
